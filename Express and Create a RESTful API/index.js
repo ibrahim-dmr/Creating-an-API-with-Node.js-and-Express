6650ef18051208bf81f832ad666936244aed7817
İbrahim Halil DEMİR <ibrahimdmr1103@gmail.com>
2023-12-27 13:32:01 +0300

    the routers has been added

--- Express and Create a RESTful API/index.js
+++ Express and Create a RESTful API/index.js
@@ -10,7 +10,30 @@ server.get("/actors", (req, res) => {
     res.status(200).json(data);
 });
 
+let next_id = 4;
+server.post("/actors",  (req, res) => {
+    let new_actor = req.body;
+    new_actor.id = next_id;
+    next.id++;
+    data.push(new_actor);
+    res.status(201).json(new_actor);
+});
+
+server.delete("/actors/:id", (req, res) => {
+    const removal_actor_id = req.params.id;
+    const removedActor = data.find(actor => actor.id === Number(removal_actor_id));
+
+    if (removedActor) {
+        data = data.filter(actor => actor.id !== Number(removal_actor_id));
+        res.status(204).end();
+    } else {
+        res.status(404).json({ errorMessage: "The actor you are trying to delete does not exist in the system." });
+    }
+});
+
+
 server.get("/actors/:id" , (req,res) =>{
+    console.log("rec.query", req.query);
     const {id} = req.params;
     const actor = data.find(actor => actor.id === parseInt(id))
     
